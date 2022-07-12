86771901d023bff59a969c4e5f3e0b1120b46046
Christian Black <blackl3@gmail.com>
2022-07-12 18:40:53 -0500

    Got joke functionality to work

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,38 @@
-import GetJokeButton from '../components/GetJokeButton';
+import { useState } from 'react';
+import getJoke from '../api/jokeData';
 
 function Home() {
+  const [value, setValue] = useState('Get Joke');
+  const [data, setData] = useState(0);
+  const handleClick = () => {
+    if (value === 'Get Joke') {
+      getJoke().then((response) => setData(response));
+      setValue('Get Punchline');
+    } else if (value === 'Get Punchline') {
+      setValue('Get Another Joke');
+    } else if (value === 'Get Another Joke') {
+      setData(0);
+      getJoke().then((response) => setData(response));
+      setValue('Get Punchline');
+    }
+  };
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <GetJokeButton title="Get Button Joke" />
-    </div>
+    <>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <h2>{value === 'Get Punchline' ? data.setup : data.delivery}</h2>
+        <button type="button" onClick={handleClick}>
+          {value}
+        </button>
+      </div>
+    </>
   );
 }
 
